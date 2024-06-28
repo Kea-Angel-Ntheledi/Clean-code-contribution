e01c33618e4743feee2f1cf9940aea449ed7ad22
Software Engineer 16 <Software.Engineer16@bafokeng.local>
2024-06-28 09:24:03 +0200

    Created index.js for 1 challenge

---
+++ index.js
@@ -0,0 +1,23 @@
+//Challange 1: For meaningful names
+class D {
+    constructor(d) {
+        this.d = d;
+    }
+
+    f() {
+        if (this.d === 0) {
+            this.t();
+        } else {
+            this.p();
+        }
+    }
+
+    t() {
+        // do something
+    }
+
+    p() {
+        // do something else
+    }
+}
+
